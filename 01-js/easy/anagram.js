d6709dfc919c28a0a9a352b07366125b9e960c6e
rjmandal <rjsanjaymandal@gmail.com>
2024-04-16 21:26:28 +0530

    easy done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,14 @@
 */
 
 function isAnagram(str1, str2) {
-
+  str1 =str1.replace(/[^a-zA-Z!]/g,"").toLowerCase();
+  str2 =str2.replace(/[^a-zA-Z!]/g,"").toLowerCase();
+  let array1 =str1.split('').sort();
+  let array2 = str2.split('').sort();
+  if(array1.length !== array2.length){
+    return false;
+  }
+  return array1.join('')===array2.join('');
 }
 
 module.exports = isAnagram;
